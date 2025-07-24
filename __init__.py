55f8a7be8c612bba52eacbbc6f0de1f100ee2035
01 <159622426+OandONE@users.noreply.github.com>
2025-07-24 19:39:30 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .Client import Client, Update
 from .filters import Filter
 from .Client import Update as Updates
-__version__="0.4"
-__all__ = ['Client', 'Update', 'Filter', 'text_filter']
+__version__="0.5"
+__all__ = ['Client', 'Update', 'Filter']
