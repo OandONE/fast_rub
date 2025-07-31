edbcc03535504080486bf56a7b7ff26256302f02
01 <159622426+OandONE@users.noreply.github.com>
2025-07-31 11:44:39 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,5 +2,8 @@ from .Client import Client, Update
 from .filters import Filter
 from .Client import Update as Updates
 from .Client import UpdateButton
-__version__="0.6"
+from .version import v
+
+__author__="Seyyed Mohamad Hosein Moosavi Raja"
+__version__=v
 __all__ = ['Client', 'Update', 'Filter']
