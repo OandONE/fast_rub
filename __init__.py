8f664db27d69a02c435cd044c5f10e0a9c6c01d3
01 <159622426+OandONE@users.noreply.github.com>
2025-08-31 11:36:12 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from .Client import Client, Update
 from .filters import Filter
-from .Client import Update as Updates
-from .Client import UpdateButton
+from .type.Update import *
+from .iniline.KeyPad import *
 from .version import v
 
 __author__="Seyyed Mohamad Hosein Moosavi Raja"
 __version__=v
-__all__ = ['Client', 'Update', 'Filter']
+__all__ = ['Client', 'Update', 'Filter', 'UpdateButton','KeyPad']
