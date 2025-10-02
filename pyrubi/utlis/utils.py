46a6da43f7128d6065231df0117aa07664dc0f78
01 <159622426+OandONE@users.noreply.github.com>
2025-10-02 20:02:16 +0330

    Update utils.py

--- pyrubi/utlis/utils.py
+++ pyrubi/utlis/utils.py
@@ -1,4 +1,3 @@
-
 from random import choices, randint
 from time import time
 from re import finditer, sub
@@ -10,65 +9,74 @@ from filetype import guess
 from os import system, chmod, remove
 from .configs import Configs
 
-class Utils:
 
+class Utils:
+    @staticmethod
     def randomTmpSession() -> str:
         return "".join(choices("abcdefghijklmnopqrstuvwxyz", k=32))
-    
+
+    @staticmethod
     def randomDeviceHash() -> str:
         return "".join(choices("0123456789", k=26))
-    
+
+    @staticmethod
     def randomRnd() -> str:
         return str(randint(-99999999, 99999999))
-    
-    def privateParse(private:str) -> str:
+
+    @staticmethod
+    def privateParse(private: str) -> str:
         if private:
             if not private.startswith("-----BEGIN RSA PRIVATE KEY-----"):
-                private = "-----BEGIN RSA PRIVATE KEY-----\\n" + private
-            
+                private = "-----BEGIN RSA PRIVATE KEY-----\n" + private
+
             if not private.endswith("-----END RSA PRIVATE KEY-----"):
-                private += "\\n-----END RSA PRIVATE KEY-----"
+                private += "\n-----END RSA PRIVATE KEY-----"
 
-            return private.replace("\\n", "\n").strip()
+            return private.replace("\n", "\n").strip()
 
+    @staticmethod
     def getState() -> int:
         return int(time()) - 150
-    
-    def phoneNumberParse(phoneNumber:str) -> str:
+
+    @staticmethod
+    def phoneNumberParse(phoneNumber: str) -> str:
         if str(phoneNumber).startswith("0"):
             phoneNumber = phoneNumber[1:]
-
         elif str(phoneNumber).startswith("98"):
             phoneNumber = phoneNumber[2:]
-
         elif str(phoneNumber).startswith("+98"):
             phoneNumber = phoneNumber[3:]
-
         return phoneNumber
-    
-    def getChatTypeByGuid(objectGuid:str) -> str:
+
+    @staticmethod
+    def getChatTypeByGuid(objectGuid: str) -> str:
         for chatType in [("u0", "User"), ("g0", "Group"), ("c0", "Channel"), ("s0", "Service"), ("b0", "Bot")]:
             if objectGuid.startswith(chatType[0]):
                 return chatType[1]
-    
-    def isChatType(chatType:str) -> bool:
+
+    @staticmethod
+    def isChatType(chatType: str) -> bool:
         chatType = chatType.lower()
         for type in ["user", "group", "channel", "service", "bot"]:
             if type == chatType:
                 return True
-            
-    def isMessageType(messageType:str) -> bool:
+
+    @staticmethod
+    def isMessageType(messageType: str) -> bool:
         messageType = messageType.lower()
         for type in ["text", "image", "video", "gif", "video message", "voice", "music", "file"]:
             if type == messageType:
                 return True
 
-    
-    def getChatTypeByLink(link:str) -> str:
-        if "rubika.ir/joing" in link: return "Group"
-        elif "rubika.ir/joinc" in link: return "Channel"
-    
-    def checkMetadata(text):
+    @staticmethod
+    def getChatTypeByLink(link: str) -> str:
+        if "rubika.ir/joing" in link:
+            return "Group"
+        elif "rubika.ir/joinc" in link:
+            return "Channel"
+
+    @staticmethod
+    def checkMetadata(text) -> tuple:
         if text is None:
             return [], ""
 
@@ -133,42 +141,46 @@ class Utils:
                 real_text = real_text.replace(f"({mentionObjects[mentionObjectIndex]})", "")
                 conflict += 6 + len(mentionObjects[mentionObjectIndex])
                 mentionObjectIndex += 1
-                
+
         return result, real_text
-    
-    def checkLink(url:str) -> dict:
+
+    @staticmethod
+    def checkLink(url: str) -> bool:
         for i in ["http:/", "https://"]:
-            if url.startswith(i): return True
+            if url.startswith(i):
+                return True
 
-    def getMimeFromByte(bytes:bytes) -> str:
-        mime = guess(bytes)
+    @staticmethod
+    def getMimeFromByte(data: bytes) -> str:
+        mime = guess(data)
         return "pyrubi" if mime is None else mime.extension
-    
-    def generateFileName(mime:str) -> str:
-        return "Pyrubi Library {}.{}".format(randint(1, 1000), mime)
-    
-    def getImageSize(bytes:bytes) -> str:
-        try:
-            from PIL import Image
-        except ImportError:
-            system("pip install pillow")
-            from PIL import Image
 
-        width, height = Image.open(BytesIO(bytes)).size
-        return width , height
-    
-    def getImageThumbnail(bytes:bytes) -> str:
+    @staticmethod
+    def generateFileName(mime: str) -> str:
+        return "Pyrubi Library {}.{}".format(randint(1, 1000), mime)
+
+    @staticmethod
+    def getImageSize(data: bytes) -> tuple:
         try:
             from PIL import Image
         except ImportError:
             system("pip install pillow")
             from PIL import Image
-            
-        image = Image.open(BytesIO(bytes))
+        width, height = Image.open(BytesIO(data)).size
+        return width, height
+
+    @staticmethod
+    def getImageThumbnail(data: bytes) -> str:
+        try:
+            from PIL import Image
+        except ImportError:
+            system("pip install pillow")
+            from PIL import Image
+        image = Image.open(BytesIO(data))
         width, height = image.size
         if height > width:
             new_height = 40
-            new_width  = round(new_height * width / height)
+            new_width = round(new_height * width / height)
         else:
             new_width = 40
             new_height = round(new_width * height / width)
@@ -176,23 +188,20 @@ class Utils:
         changed_image = BytesIO()
         image.save(changed_image, format="PNG")
         return b64encode(changed_image.getvalue()).decode("UTF-8")
-    
-    def getVideoData(bytes:bytes) -> list:
+
+    @staticmethod
+    def getVideoData(data: bytes) -> tuple:
         try:
             from moviepy.editor import VideoFileClip
-
             with NamedTemporaryFile(delete=False, dir=".") as temp_video:
-                temp_video.write(bytes)
+                temp_video.write(data)
                 temp_path = temp_video.name
-
             chmod(temp_path, 0o777)
-
             try:
                 from PIL import Image
             except ImportError:
                 system("pip install pillow")
                 from PIL import Image
-
             with VideoFileClip(temp_path) as clip:
                 duration = clip.duration
                 resolution = clip.size
@@ -202,33 +211,32 @@ class Utils:
                 thumbnail_image.save(thumbnail_buffer, format="JPEG")
                 thumbnail_b64 = b64encode(thumbnail_buffer.getvalue()).decode("UTF-8")
                 clip.close()
-
             remove(temp_path)
-
             return thumbnail_b64, resolution, duration
         except ImportError:
-            print(Colors.YELLOW + "Can't get video data! Please install the moviepy library by following command:\npip install moviepy" + Colors.RESET)
+            print("Can't get video data! Please install the moviepy library with: pip install moviepy")
             return Configs.defaultTumbInline, [900, 720], 1
         except:
             return Configs.defaultTumbInline, [900, 720], 1
-        
-    def getVoiceDuration(bytes:bytes) -> int:
+
+    @staticmethod
+    def getVoiceDuration(data: bytes) -> int:
         file = BytesIO()
-        file.write(bytes)
+        file.write(data)
         file.seek(0)
         return mp3.MP3(file).info.length
-    
-    def getMusicArtist(bytes:bytes) -> str:
-        try:
-            audio = File(BytesIO(bytes), easy=True)
 
+    @staticmethod
+    def getMusicArtist(data: bytes) -> str:
+        try:
+            audio = File(BytesIO(data), easy=True)
             if audio and "artist" in audio:
                 return audio["artist"][0]
-            
             return "pyrubi"
         except Exception:
             return "pyrubi"
-        
+
+
 class Colors:
     RESET = "\033[0m"
     BLACK = "\033[30m"
@@ -239,7 +247,6 @@ class Colors:
     MAGENTA = "\033[35m"
     CYAN = "\033[36m"
     WHITE = "\033[37m"
-
     BG_RESET = "\033[49m"
     BG_BLACK = "\033[40m"
     BG_RED = "\033[41m"
