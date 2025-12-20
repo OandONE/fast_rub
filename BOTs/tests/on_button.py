be6d52002317067616f5cab27d497de5bfa78870
01 <159622426+OandONE@users.noreply.github.com>
2025-12-20 18:57:09 +0330

    Update on_button.py

--- BOTs/tests/on_button.py
+++ BOTs/tests/on_button.py
@@ -1,5 +1,6 @@
 from fast_rub import Client
 from fast_rub.type import UpdateButton
+import asyncio
 
 bot = Client("test") # برای استفاده از وبهوک فست روب
 bot = Client("test",use_to_fastrub_webhook_on_button="https://Test.com/webhook") # برای استفاده از وبهوک اختصاصی(مقدار use_to_fastrub_webhook_on_button باید وبهوک باشد)
@@ -9,4 +10,4 @@ async def test(msg: UpdateButton):
     print(msg)
     await msg.send_text("this is a text from fast rub")
 
-bot.run()
+asyncio.run(bot.run())
