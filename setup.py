4ca857fd87d09820c068dc12ef6291ad05b56342
01 <159622426+OandONE@users.noreply.github.com>
2025-12-31 00:15:23 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastrub",
-    version="2.6.0",
+    version="2.6.1",
     author="seyyed mohamad hosein moosavi raja(01)",
     author_email="mohamadhosein159159@gmail.com",
     description="The fastest library for writing Rubika messenger bots both synchronously and asynchronously ⚡",
@@ -23,3 +23,4 @@ setup(
     ],
     license="MIT"
 )
+
