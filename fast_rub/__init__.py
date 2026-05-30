37601785aecfa5ba421efc4d0cc6511b95f4ded4
01 <159622426+OandONE@users.noreply.github.com>
2026-05-30 16:29:09 +0330

    Update __init__.py

--- fast_rub/__init__.py
+++ fast_rub/__init__.py
@@ -1,9 +1,10 @@
-from .core.client import Client, Robot, Bot, BotApi, BotApiClient
+from .core import Client, Robot, Bot, BotApi, BotApiClient, Conversation
 from .utils import filters
 from .utils.version import v
 from .utils import WaitManager
 from .utils.metadata import MetaData
 from .button.key_pad import KeyPad
+from .db.database import DataBase
 
 __author__ = "Seyyed Mohamad Hosein Moosavi Raja"
 __version__ = v
