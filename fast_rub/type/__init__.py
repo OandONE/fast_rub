c458f09b02938866fb50d310c2eb729a3bbccbea
01 <159622426+OandONE@users.noreply.github.com>
2026-05-30 16:47:57 +0330

    Update __init__.py

--- fast_rub/type/__init__.py
+++ fast_rub/type/__init__.py
@@ -2,3 +2,4 @@ from .update import (Update, UpdateButton, Message, Updates, UpdateInline,
     InlineUpdate, InlineMessage, MessageInline)
 from .errors import *
 from .prop_update import msg_update
+from .webhook import WebhookConfig
