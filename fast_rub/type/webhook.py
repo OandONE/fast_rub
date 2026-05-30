74c0385a316e15f95d9ee406cd811390602a5caa
01 <159622426+OandONE@users.noreply.github.com>
2026-05-30 21:59:09 +0330

    Update webhook.py

--- fast_rub/type/webhook.py
+++ fast_rub/type/webhook.py
@@ -9,6 +9,5 @@ class WebhookConfig:
     host: str = "0.0.0.0"               # آدرس گوش دادن
     port: int = 443                      # پورت (پیش‌فرض ۴۴۳)
     path_prefix: str = "/webhook"        # پیشوند مسیر
-    secret_token: Optional[str] = None   # توکن امنیتی
     backend: Literal["fastapi", "flask"] = "fastapi" # بک اند
     use_token_in_url: bool = False      # استفاده توکن در آدرس
