f0ae2ba16b6b853e2a714e16ba4370650e4ffbc6
01 <159622426+OandONE@users.noreply.github.com>
2026-06-19 16:03:00 +0330

    Update __init__.py

--- fast_rub/types/__init__.py
+++ fast_rub/types/__init__.py
@@ -1,8 +1,7 @@
-from . import *
-from .wait_manager import WaitManager
-from .antispam import AntiSpam
-from .utils import Utils
-from .cache import Cache
-from .snapshot import SnapshotManager
-from .template import FileOpen, TemplateEngine
-from .run_filter import _run_filter
+from .update import (Update, Message, Updates)
+from .update_button import (UpdateButton, UpdateInline,
+    InlineUpdate, InlineMessage, MessageInline)
+from .errors import *
+from .prop_update import msg_update
+from .webhook import WebhookConfig
+from .forms import *
