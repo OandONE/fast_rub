6fd8d477d7861374ce43d79a17a42fca9849045e
01 <159622426+OandONE@users.noreply.github.com>
2026-06-19 15:55:11 +0330

    Update __init__.py

--- fast_rub/core/__init__.py
+++ fast_rub/core/__init__.py
@@ -1,3 +1,5 @@
 from .async_sync import *
 from .client import Client, BotApi, Robot, Bot, BotApiClient
 from .conversation import Conversation
+from .scheduler import Scheduler
+from .webhook_server import WebhookServer
